3ddd9abc7b647f6ba16838c30bd559b67829eeb2
CRod-Dev <crod.dev@gmail.com>
2019-05-07 10:28:59 -0600

    Initial commit

---
+++ DutchTreat/Data/DutchContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace DutchTreat.Data
+{
+  public class DutchContext : IdentityDbContext<StoreUser>
+  {
+    public DutchContext(DbContextOptions<DutchContext> options): base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+  }
+}
